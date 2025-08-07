959ffac3ac675467eda6720d3c2efd01f1a23033
William Guilherme Dahlke <williamgdahlke@gmail.com>
2025-08-06 23:11:03 -0300

    feat: creating the first component - part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import logo from './imagens/logo.svg';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img> 
-          <p><strong>Alura Books</strong></p> 
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
