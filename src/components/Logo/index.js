a956383deb946da4f9b86f1b00f831241ac79741
William Guilherme Dahlke <williamgd@weg.net>
2025-08-07 17:33:35 -0300

    feat: creating the first component - part 3

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,11 @@ import './estilo.css'
 function Logo(){
     return (
         <div className='logo'>
-          <img src={logo} alt='logo'></img> 
+          <img 
+            src={logo} 
+            alt='logo' 
+            className='logo-img'
+          ></img> 
           <p><strong>Alura Books</strong></p> 
         </div>
     )
